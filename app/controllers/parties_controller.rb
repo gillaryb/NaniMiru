a479eb65622f919a78730bd0f6a9f03430ebc492
Jim Deeth <james.deeth@gmail.com>
2023-02-28 15:38:30 +0900

    showing parties

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -3,4 +3,9 @@ class PartiesController < ApplicationController
   def index
     @parties = Party.all
   end
+
+  def show
+    @party = Party.find(params[:id])
+    @user = current_user
+  end
 end
