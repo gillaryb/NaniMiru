84c45148e354a0d7ceddc4f8b9efdf98b0442004
Jim Deeth <james.deeth@gmail.com>
2023-02-28 18:06:29 +0900

    swipes saving

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   def index
     @movies = policy_scope(Movie)
+    @swipe = Swipe.new
+    @party = Party.find(params[:party_id])
   end
 end
--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -1,6 +1,6 @@
 class SwipesController < ApplicationController
   def new
-    @movie = Uovie.find(params[:movie_id])
+    @movie = Movie.find(params[:movie_id])
     @user = User.find(params[:user_id])
     @party = Party.find(params[:party_id])
     @swipe = Swipe.new
@@ -8,10 +8,19 @@ class SwipesController < ApplicationController
   end
 
   def create
+    @party = Party.find(params[:party_id])
     @swipe = Swipe.new(swipe_params)
-    @swipe.movie = @movie
     @swipe.party = @party
     @swipe.user = current_user
     authorize @swipe
+    @swipe.save
+    redirect_to party_movies_path(@party)
   end
+
+  private
+
+  def swipe_params
+    params.require(:swipe).permit(:movie_id, :status)
+  end
+
 end
