9d8bdb0dd92157e2106e4966aa7d25d743bc4cad
Anri Tomita <12bm834@gmail.com>
2023-03-01 11:14:46 +0900

    added new pag

--- app/models/party.rb
+++ app/models/party.rb
@@ -6,6 +6,7 @@ class Party < ApplicationRecord
   has_many :swipes, dependent: :destroy
   has_many :users, through: :memberships
 
+
   def owner
     memberships.find_by(creator: true)&.user
   end
