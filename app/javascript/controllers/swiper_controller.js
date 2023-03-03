c4a56cb62ea50bcad56055dbbe2ae159fc6aa99b
Gilary Bacnis <gilarybacnis@gmail.com>
2023-03-03 15:05:23 +0900

    modified swiping

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -8,6 +8,26 @@ export default class extends Controller {
     const swiper = new Swiper(".mySwiper", {
       effect: "cards",
       grabCursor: true,
+      on: {
+        init: function (event) {
+          console.log('swiper init', event);
+       },
+       transitionEnd: function (event) {
+           console.log('swiper change', event.swipeDirection, event.slides[event.previousIndex].id);
+          //  swiper.removeSlide(event.previousIndex)
+        }
+      },
     });
+
+    // swiper.on('slideChange', (event) => {
+    //   if (this.flag === false ) {
+    //     console.log('slide changed', event);
+    //     swiper.removeSlide(event.previousIndex)
+    //     this.flag = true
+    //     setTimeout(() => {
+    //       this.flag = false
+    //     }, 2000);
+    //   }
+    // });
   }
 }
