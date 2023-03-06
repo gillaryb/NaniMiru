37d41b9f5c9556a248a273774717f1dce8d09616
Jim Deeth <james.deeth@gmail.com>
2023-03-06 12:02:55 +0900

    added models for genre and moviegenre

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_021506) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_025023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.integer "api_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "memberships", force: :cascade do |t|
     t.bigint "user_id", null: false
