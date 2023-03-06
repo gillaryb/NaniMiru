d210a32c27c06d1bd41a314dbc972220146ee31a
Chieri Kii <kiichieri@gmail.com>
2023-03-06 17:55:55 +0900

    fix-the-clickcopy

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,16 +1,44 @@
-import Clipboard from 'stimulus-clipboard'
+// import Clipboard from "stimulus-clipboard"
 
-export default class extends Clipboard {
-  connect() {
-    super.connect()
-    console.log('Do what you want here.')
-  }
+// export default class extends Clipboard {
+//   connect() {
+//     super.connect()
+//     console.log('Do what you want here.')
+//     console.log(this.buttonTarget)
+//   }
 
-  // Function to override on copy.
-  copy() {}
+//   // Function to override on copy.
+//   copy() {
+//     console.log("I am in copy");
 
-  // Function to override when to input is copied.
-  copied() {
-    //
+//   }
+
+//   // Function to override when to input is copied.
+//   copied() {
+//     console.log("I am in copied");
+//     //
+//   }
+// }
+
+// app/javascript/controllers/clipboard_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["source", "trigger"]
+
+  copy(event) {
+    event.preventDefault()
+    navigator.clipboard.writeText(this.sourceTarget.value)
+
+    this.sourceTarget.focus()
+    var triggerElement = this.triggerTarget
+    var initialHTML = triggerElement.innerHTML
+    triggerElement.innerHTML = "<span style='color:#F30AB2;'>Copied</span>"
+    setTimeout(() => {
+      triggerElement.innerHTML = initialHTML
+      // unfocus
+      this.sourceTarget.blur()
+    }, 2000)
+
   }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -15,3 +15,25 @@ application.register("popover", PopoverController)
 
 import SwiperController from "./swiper_controller"
 application.register("swiper", SwiperController)
+
+import Clipboard from "stimulus-clipboard"
+
+export default class extends Clipboard {
+  connect() {
+    super.connect()
+    console.log('Do what you want here.')
+    console.log(this.buttonTarget)
+  }
+
+  // Function to override on copy.
+  copy() {
+    console.log("I am in copy");
+
+  }
+
+  // Function to override when to input is copied.
+  copied() {
+    console.log("I am in copied");
+    //
+  }
+}
