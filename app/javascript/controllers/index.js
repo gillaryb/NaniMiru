a8997b1cd519f6a6ab27af5090a72097e6f37199
Chieri Kii <kiichieri@gmail.com>
2023-03-07 20:01:26 +0900

    navbarfix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,26 +10,4 @@ import SwiperController from "./swiper_controller"
 application.register("swiper", SwiperController)
 
 import ClipboardController from './clipboard_controller'
-application.register('clipboard', Clipboard)
-
-import Clipboard from "stimulus-clipboard"
-
-export default class extends Clipboard {
-  connect() {
-    super.connect()
-    console.log('Do what you want here.')
-    console.log(this.buttonTarget)
-  }
-
-  // Function to override on copy.
-  copy() {
-    console.log("I am in copy");
-
-  }
-
-  // Function to override when to input is copied.
-  copied() {
-    console.log("I am in copied");
-    //
-  }
-}
+application.register('clipboard', ClipboardController)
