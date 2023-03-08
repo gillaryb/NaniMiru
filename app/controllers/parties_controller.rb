7c07609f6350065a074e127c3ed10ea3c23d0927
Chieri Kii <kiichieri@gmail.com>
2023-03-08 15:43:34 +0900

    logoupdate

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -15,10 +15,10 @@ class PartiesController < ApplicationController
     authorize @party
     if @party.save
       Membership.create(user: current_user, party: @party)
-      
+
       redirect_to party_path(@party)
     else
-      
+
       @genres = Genre.all
       render :new, status: :unprocessable_entity
     end
